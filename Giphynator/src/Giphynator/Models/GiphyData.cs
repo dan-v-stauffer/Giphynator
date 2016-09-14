354bc46eaf6b2f9861df46cb7b9f3e2e8c2b2fa0
Dan Stauffer <Daniel.Stauffer@lendingtree.com>
2016-09-14 19:24:28 -0400

    Initial Commit

---
+++ Giphynator/src/Giphynator/Models/GiphyData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Giphynator.Models
+{
+    public class GiphyData
+    {
+        [JsonProperty("data")]
+        public List<GiphyItem> data { get; set; }
+    }
+}
--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
 #  This script should start up your service on port 8080
+
+# start server
+cd ~/clara-challenge-backend/Giphynator/src/Giphynator
+dotnet run
+
